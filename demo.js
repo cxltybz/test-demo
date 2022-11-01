046db78609cf06ab373486ef570514fe9dc95dcb
cxltybz <1324071204@qq.com>
2022-11-02 00:47:44 +0800

    第一次提交文件

---
+++ demo.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+    let box=document.getElementsByClassName('box')[0];
+    console.log(box);
+    box.addEventListener('click',function(){
+        alert('代码测试');
+       
+    })
+}   
